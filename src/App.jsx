507873589a22c8b59bef2ebf7e4e8e58cf1e1268
Adarsh Patil <adarsh.patil81359@outlook.com>
2025-09-16 21:05:13 +0530

    feat: Integrate useAntiInspect hook into App component for enhanced security

--- src/App.jsx
+++ src/App.jsx
@@ -2,8 +2,11 @@ import { RouterProvider } from "react-router-dom";
 import router from "./routes/router";
 import ResponsiveToaster from "./components/ResponsiveToaster";
 import React from "react";
+import useAntiInspect from "./hooks/useAntiInspect";
 
 const App = () => {
+	useAntiInspect();
+
 	return (
 		<React.Fragment>
 			<RouterProvider router={router} />
